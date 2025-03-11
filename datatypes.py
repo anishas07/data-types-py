5137aeefc47f2f1b99e49367cca028759df03d56
anishas07 <Studytea95@gmail.com>
2025-03-11 19:19:14 +1030

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,16 @@
+list1 = ["mango", "strawberry", "blueberry", "guava", "watermelon"]
+print(list1[1]) #indexing strawberry 
+print(list1[0:4])  #slicing
+print("List1", len(list1)) #length
+list1.append("apple") #append
+print(list1)
+list1.remove("watermelon") #remove an element
+print(list1)
+list1.sort()   #sort element
+print(list1)
+list1.pop(3) #pop
+print(list1)
+list1.reverse() #reverse
+print(list1)
+list1.clear() #delete
+print(list1)
